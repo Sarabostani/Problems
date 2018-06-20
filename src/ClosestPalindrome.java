f06e6b96ac979935e97bcb396bbc9ec5e35115e5
Houman Sarabostani <sarabostani@outlook.com>
2018-06-19 21:03:00 -0700

    space indentation

--- src/ClosestPalindrome.java
+++ src/ClosestPalindrome.java
@@ -1,38 +1,38 @@
 
 public class ClosestPalindrome {
-	public static void Run() {
-		var input = "807045053224792883";
-		System.out.println(String.format("Is '%s' palindrome: %s", input, isPalindrome(input)));
-		System.out.println(String.format("Closes palindrome to '%s' is: %s", input, closestPalindrome(input)));		
-	}
+    public static void Run() {
+        var input = "807045053224792883";
+        System.out.println(String.format("Is '%s' palindrome: %s", input, isPalindrome(input)));
+        System.out.println(String.format("Closes palindrome to '%s' is: %s", input, closestPalindrome(input)));
+    }
 
-	private static String closestPalindrome(String input) {
+    private static String closestPalindrome(String input) {
 
-		Long value = Long.parseLong(input);
-		int boundary = 1;
-		String candidate = input;
+        Long value = Long.parseLong(input);
+        int boundary = 1;
+        String candidate = input;
 
-		while (boundary <= value) {
-			candidate = String.valueOf(value - boundary);
-			if (isPalindrome(candidate)) {
-				break;
-			}
-			candidate = String.valueOf(value + boundary);
-			if (isPalindrome(candidate)) {
-				break;
-			}
-			boundary++;
-		}
-		
-		return candidate;
-	}
+        while (boundary <= value) {
+            candidate = String.valueOf(value - boundary);
+            if (isPalindrome(candidate)) {
+                break;
+            }
+            candidate = String.valueOf(value + boundary);
+            if (isPalindrome(candidate)) {
+                break;
+            }
+            boundary++;
+        }
 
-	private static boolean isPalindrome(String input) {
-		for (int i = 0, j = input.length() - 1; i < input.length() / 2; i++, j--) {
-			if (input.charAt(i) != input.charAt(j)) {
-				return false;
-			}
-		}
-		return true;
-	}
+        return candidate;
+    }
+
+    private static boolean isPalindrome(String input) {
+        for (int i = 0, j = input.length() - 1; i < input.length() / 2; i++, j--) {
+            if (input.charAt(i) != input.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		// FindDuplicates.Run();
-		//ClosestPalindrome.Run();
-		//SortLogs.Run();
-	    Morse.Run();
-	}
+    public static void main(String[] args) {
+        // FindDuplicates.Run();
+        // ClosestPalindrome.Run();
+        // SortLogs.Run();
+        Morse.Run();
+    }
 
 }
